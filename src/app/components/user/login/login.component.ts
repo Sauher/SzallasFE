a482e99b2352de3c13efdd00bdcd788eac136bf1
Sauher <schaueroliver@turr.hu>
2025-12-09 11:59:26 +0100

    Login script

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { APIService } from '../../../services/api.service';
+import { AuthService } from '../../../services/auth.service';
+import { MessageService } from '../../../services/message.service';
+
+interface User {
+  name: string;
+  email: string;
+  password: string;
+  confirm: string;
+  role: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -8,5 +20,36 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  user: User = {
+    name: '',
+    email: '',
+    password: '',
+    confirm: '',
+    role: '',
+  }
 
+  rememberMe: boolean = false;
+
+  constructor(
+    private api: APIService,
+    private auth: AuthService,
+    private message: MessageService,
+    private router: Router
+  ) {}
+
+  login() {
+    this.api.Login('users', this.user).then(res => {
+      if (res.status == 500) {
+        this.message.show('danger', 'Hiba', "Hiba van a rendszerben")
+        return
+      }
+      if (this.rememberMe) {
+        this.auth.storeUser(JSON.stringify(res.data))
+      }
+
+      this.auth.login(JSON.stringify(res.data));
+      this.router.navigate(['/szallasok']);
+      this.message.show('success', 'Siker', res.message);
+    })
+  }
 }
