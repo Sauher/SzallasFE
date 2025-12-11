ee2e1e23fc1b229778376a220cf8f0f7623a0f1a
Csongoranevem <czakocsongor@turr.hu>
2025-12-11 12:23:50 +0100

    navbar animáció

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/user/login/login.component';
 import { NavbarComponent } from './components/system/navbar/navbar.component';
@@ -13,12 +13,32 @@ import { HeaderComponent } from './components/system/header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   app_title = 'Szállás foglaló';
   app_authors = 'Schauer Olivér, Czakó Csongor - 2025';
   about = '2024-ben indult projektünk célja, hogy megkönnyítsük a szállásfoglalást mindenki számára. Ez egy szállás foglaló alkalmazás, ahol különböző szálláshelyek közül választhatsz és foglalhatsz le szobákat egyszerűen és gyorsan. Regisztrálj, jelentkezz be, és kezdj el felfedezni a legjobb ajánlatokat!';
   address = '6500 Baja, Bácska tér 1.';
   app_email = 'szallasfogalo@example.com';
+
+  //Navbar sticky animáció
+  @ViewChild('detect', { static: true }) sentinel!: ElementRef;
+  @ViewChild('navbar', { static: true }) navbar!: ElementRef;
+
+  isStuck = false;
+
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        this.isStuck = entry.intersectionRatio === 0;
+      },
+      {
+        threshold: 0,
+        rootMargin: '0px'
+      }
+    );
+
+    observer.observe(this.sentinel.nativeElement);
+  }
 
 
 }
